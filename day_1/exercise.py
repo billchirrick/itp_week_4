237b37ff307312a4c7c5073987a4ef161194f898
Bill Chirrick <billchirrick@gmail.com>
2021-09-14 19:06:56 -0700

    day2

--- day_1/exercise.py
+++ day_1/exercise.py
@@ -2,43 +2,101 @@
 
 # https://data.messari.io/api/v2/assets
 
+# import requests
+# import json
+# import openpyxl
+
+# wb = openpyxl.load_workbook("output.xlsx")
+# sheet = wb["Sheet1"]
+
+# def get_data(url):
+#     response = requests.get(url)
+#     # print(response)
+#     json_result = response.text
+#     # print(json_result)
+#     # print(type(json_result))
+#     clean_data = json.loads(json_result)
+#     result = clean_data["data"]
+#     return result
+
+# row = 1
+
+# def write_data(result):
+#     global row
+#     for messari in result:
+#         sheet['A' + str(row)] = messari['symbol']
+#         sheet['B' + str(row)] = messari['name']
+#         sheet['C' + str(row)] = messari['metrics']['market_data']['price_usd']
+#         row+=1
+
+
+# result_1 = get_data("https://data.messari.io/api/v2/assets")
+# result_2 = get_data("https://data.messari.io/api/v2/assets?page=2")
+# result_3 = get_data("https://data.messari.io/api/v2/assets?page=3")
+
+# write_data(result_1)
+# write_data(result_2)
+# write_data(result_3)
+# write_data(get_data("https://data.messari.io/api/v2/assets?page=4"))
+
+
+
+# wb.save("output.xlsx")
+
+
+# ITP Week 4 Day 1 Exercise
+
+# https://data.messari.io/api/v2/assets
+
+
+
 import requests
 import json
 import openpyxl
-
-wb = openpyxl.load_workbook("output.xlsx")
-sheet = wb["Sheet1"]
-
-def get_data(url):
-    response = requests.get(url)
-    # print(response)
-    json_result = response.text
-    # print(json_result)
-    # print(type(json_result))
-    clean_data = json.loads(json_result)
-    result = clean_data["data"]
-    return result
-
-row = 1
-
-def write_data(result):
-    global row
-    for messari in result:
-        sheet['A' + str(row)] = messari['symbol']
-        sheet['B' + str(row)] = messari['name']
-        sheet['C' + str(row)] = messari['metrics']['market_data']['price_usd']
-        row+=1
+from openpyxl.styles import Font
 
 
-result_1 = get_data("https://data.messari.io/api/v2/assets")
-result_2 = get_data("https://data.messari.io/api/v2/assets?page=2")
-result_3 = get_data("https://data.messari.io/api/v2/assets?page=3")
+data = requests.get('https://data.messari.io/api/v2/assets')
+beautify = json.loads(data.text)
 
-write_data(result_1)
-write_data(result_2)
-write_data(result_3)
-write_data(get_data("https://data.messari.io/api/v2/assets?page=4"))
+symbol = beautify['data']
+
+roi = beautify['data'][0]['metrics']['roi_data']['percent_change_last_1_week'] 
+
+# print(roi)
+# print(symbol)
+
+
+wb = openpyxl.load_workbook('C:\\Users\\GorkhaliSquad\\Documents\\VetsInTech\\itp_week_4\\day_1\\output.xlsx')
+
+sheet = wb['Sheet1']
+
+sheet['A1'] = "Symbol"
+sheet['A1'].font = Font(bold = True)
+sheet['B1'] =  'ROI'
+sheet['B1'].font = Font(bold = True)
+
+sym_counter = 0
+sheet_counter = 2
+
+for index in range(20):
+    symbol_list = beautify['data'][sym_counter]['symbol']
+    sheet['A' + str(sheet_counter)] = symbol_list
+    sheet_counter += 1
+    sym_counter += 1
+
+
+sheet2_counter = 2
+roi_counter = 0
+
+for item in range(20):
+    roi_list = beautify['data'][roi_counter]['metrics']['roi_data']['percent_change_last_1_week'] 
+    sheet['B' + str(sheet2_counter)] = roi_list
+    sheet2_counter += 1
+    roi_counter += 1
 
 
 
-wb.save("output.xlsx")
+
+
+wb.save('C:\\Users\\GorkhaliSquad\\Documents\\VetsInTech\\itp_week_4\\day_1\\output.xlsx')
--- day_2/exercise.py
+++ day_2/exercise.py
@@ -1,1 +1,95 @@
+# f string
+# url = f"https://pokeapi.co/api/v2/pokemon/{num}/"
+
 # ITP Week 4 Day 2 Exercise
+
+#Today we will pull information from the Pokemon api, put it into a dictionary, and then put that info into a new Excel file.  We will write the pseudocode as a group in class.  Be sure to follow the pseudocode, break your problems down into smaller pieces, and consult the documentation whenever you get stuck: https://pokeapi.co/api/v2/pokemon
+
+#PSEUDO-CODE:
+
+#GET NAME AND ABILITY FROM API
+#PUT INFO IN DICTIONARY
+#ADD THE DICTIONARY TO A NEW EXCEL WORKBOOK
+
+#imports:
+    #json
+    #openpyxl
+
+#Input
+    #json file from pokemon api
+    #workbook
+
+#Assign response to variable
+
+#Create workbook
+    #get workbook from openpy
+    #load workbook
+    #assign workbook to variable
+    
+#Create Worksheet
+    #assign sheet to variable
+
+#Create a dictionary, assign to variable
+
+#FUNCTION BODY
+    #Convert response to json file
+        #clean data(response)
+            #json.loads(response.text)
+
+    #Iterate over response
+        #for each pokemon in response
+            #variable key = pokemon.name
+            #variable value = pokemon.abilites
+            #append {key/value} pair to dictionary
+
+    #Iterate over dictionary
+        #for each item in dictionary
+            #assign dictionary values to rows & cols
+                #Write Name to Cell
+                #Write Abilities to Cell
+
+#Output
+    #Workbook
+
+# pokemon = {
+#     bulbasour : {
+#         "name": "pokemon_name",
+#         "abilities": ["ability1", "ability2"]
+#     },
+#     pikachu : {
+#         "name": "pokemon_name",
+#         "abilities": ["ability1", "ability2"]
+#     }
+# }
+
+
+#imports:
+    #json
+    #openpyxl
+import openpyxl
+import json
+import requests
+from requests.api import request
+from pprint import pprint
+#Input
+    #json file from pokemon api
+    #workbook
+    # https://pokeapi.co/api/v2/pokemon
+    # https://pokeapi.co/api/v2/pokemon/1/
+# url = "hello " + name + ", " , str(age)
+# url = f"hello {name}, {age}"    F-strings
+pokemon = {}
+for num in range(1, 5):
+    url = f"https://pokeapi.co/api/v2/pokemon/{num}/"
+    #print(url)
+    response = requests.get(url)
+    json_data = json.loads(response.text)
+    #forms = json_data['forms'][0]['name']
+    name = json_data['forms'][num]['name']
+    abilities = json_data['abilities']
+    for ability in abilities:
+        print(ability['ability']['name'])
+    # start to populate pokemon dict.
+    pokemon['name'] = name
+    pokemon['abilities']
+    pprint(forms)
